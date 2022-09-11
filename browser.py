00a3ee2bf212dd72242ce02e7bfe0ba96bde276f
victorkashima <victorkashima04@gmail.com>
2022-09-11 15:11:52 -0300

    up

---
+++ browser.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    # Can be "msedge", "chrome-beta", "msedge-beta", "msedge-dev", etc.
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.youtube.com/")
+    page.fill("input[id='search']", "SAVEIRO REBAIXADA")
+    page.click("button[id='search-icon-legacy']")
+    print(page.title())
+    page.wait_for_timeout(5000)
+    # browser.close()
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-from zipapp import get_interpreter
 import speech_recognition as sr
+import sounddevice
+from playsound import playsound
 
-#Reconhecedor
-r = sr.Recognizer()
-m = sr.Microphone()
-print(r)
-with m as source:
-    print(source)
-    audio = r.listen(source)
-    try:
-        said = r.recognize_google(audio)
-        print(said)
-    except Exception as e:
-        print("Exception: " + str(e))
+def read_mic():
+    mic = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        mic.adjust_for_ambient_noise(source)
+
+        print("Diga")
+
+        audio = mic.listen(source)
+
+        sentence = mic.recognize_google(audio, language='pt-BR')
+
+        print(sentence)
+
+        # if "acelera" in sentence:
+        #     playsound("/home/vitindafixa/Downloads/lambo.mp3")
+
+while True:
+    read_mic()
